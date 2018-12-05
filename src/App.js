f5670cb0bd73231322e8516d065d1b5fb2f70296
Fabien Raymond <voortexxx@gmail.com>
2018-12-05 17:28:35 +0100

    working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import MyMap from "./MyMap";
+import { OpenStreetMapProvider } from "leaflet-geosearch";
+
+const provider = new OpenStreetMapProvider();
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      result: [],
+      adressQuery: "",
+      resultOpen: false
+    };
+  }
+
+  handleInput = event => {
+    const request = event.target.value;
+    provider.search({ query: request }).then(results => {
+      console.log(request);
+      this.setState({
+        result: results,
+        resultOpen: true,
+        adressQuery: request
+      });
+    });
+  };
+
+  chooseAdress = adress =>
+    this.setState({ adressQuery: adress, resultOpen: false });
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form>
+          <input
+            type="text"
+            onChange={event => this.handleInput(event)}
+            placeholder="search"
+            value={this.state.adressQuery}
+          />
+        </form>
+        {this.state.resultOpen && (
+          <div className="result">
+            {this.state.result.map((adress, index) => (
+              <p key={index} onClick={() => this.chooseAdress(adress.label)}>
+                {adress.label}
+              </p>
+            ))}
+          </div>
+        )}
+        <MyMap />
       </div>
     );
   }
---
+++ src/Search.js
@@ -0,0 +1,20 @@
+import { MapControl, withLeaflet } from "react-leaflet";
+import { GeoSearchControl, OpenStreetMapProvider } from "leaflet-geosearch";
+
+class Search extends MapControl {
+  createLeafletElement() {
+    return GeoSearchControl({
+      provider: new OpenStreetMapProvider(),
+      style: "bar",
+      showMarker: true,
+      showPopup: false,
+      autoClose: true,
+      retainZoomLevel: false,
+      animateZoom: true,
+      keepResult: false,
+      searchLabel: "search"
+    });
+  }
+}
+
+export default withLeaflet(Search);
